6eff554d16712e50d383a5a14921419e5e341323
chudnofskyATD <mpereannor@gmail.com>
2019-11-05 20:35:04 +0000

    CREATE/post request created and tested successfully

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -31,20 +31,33 @@ router.get('/:id', (req, res) => {
     });
 });
 */
-
-// router.post('/', (req, res) => {
-//     const carData = req.body;
-//     db('cars').insert(carData)
-//     .then(ids => {
-//       db('fruits').where({ id: ids[0] })
-//       .then(newFruitEntry => {
-//         res.status(201).json(newFruitEntry);
-//       });
-//     })
-//     .catch (err => {
-//       console.log('POST error', err);
-//       res.status(500).json({ message: "Failed to store data" });
-//     });
-//   });
+//insert new data 
+router.post('/', (req, res) => {
+    const carData =req.body;
+    db('cars')
+    .insert(carData)
+    .then(newCarEntry =>  {
+        res.status(201).json(newCarEntry);
+    })
+    .catch( error => {
+        res.status(500).json(`FAILED to store data :` + error.message)
+    })
+})
+/*
+router.post('/', (req, res) => {
+    const fruitData = req.body;
+    db('fruits').insert(fruitData)
+    .then(ids => {
+      db('fruits').where({ id: ids[0] })
+      .then(newFruitEntry => {
+        res.status(201).json(newFruitEntry);
+      });
+    })
+    .catch (err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+  });
+*/
 
 module.exports = router; 
--- data/migrations/20191105152544_build-cars-table.js
+++ data/migrations/20191105152544_build-cars-table.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     table.string('vin', 17).unique().notNullable();
     table.text('make', 128).notNullable();
     table.text('model', 128).notNullable();
-    table.decimal('mileage').notNullable();
+    table.integer('mileage').notNullable();
     table.text('transmissionType', 128);
     table.text('title', 128);
   })
